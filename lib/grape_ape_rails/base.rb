f5e6048b79f03c17bf7698b43340b707b22a7dd1
Matt E. Patterson <madraziel@gmail.com>
2015-06-29 08:53:08 -0500

    remove rescue all to avoid weird bug they haven't fixed in grape yet

--- lib/grape_ape_rails/base.rb
+++ lib/grape_ape_rails/base.rb
@@ -17,9 +17,9 @@ module GrapeApeRails
       end
 
       # global exception handler, used for error notifications
-      rescue_from :all do |e|
-        error_response(message: { error: { code: "INTERNAL_SERVER_ERROR", message: e.message } }, status: 500)
-      end
+      # rescue_from :all do |e|
+      #   error_response(message: { error: { code: "INTERNAL_SERVER_ERROR", message: e.message } }, status: 500)
+      # end
 
     end
 
